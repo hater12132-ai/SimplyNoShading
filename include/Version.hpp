784a52410a9be018d353f4886f14c51dad6f9187
bountyzz37 <bountyzz37@gmail.com>
2026-09-20 13:21:52 +0200

    BactroNative 1.2.5: 1.26.51.1 signatures for Fast Containers

--- include/Version.hpp
+++ include/Version.hpp
@@ -4,6 +4,6 @@ namespace bactro {
 inline constexpr std::string_view Name = "BactroNative";
 inline constexpr std::string_view Author = "hater12132-ai";
 inline constexpr std::string_view Description = "Full BedrockTools 1.26.5x signature table + Performance + Fast Containers.";
-inline constexpr std::string_view Version = "1.2.4";
+inline constexpr std::string_view Version = "1.2.5";
 inline constexpr std::string_view Library = "libBactroNative.so";
 }
